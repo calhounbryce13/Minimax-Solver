258318bec025d813e2e7a92c9ca2660f84970250
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2026-03-27 15:04:08 -0700

    start and reset controls

--- src/script.js
+++ src/script.js
@@ -1,19 +1,12 @@
 
-
-
-
+'use strict';
 
 document.addEventListener("DOMContentLoaded", () => {
 
     check_local_storage();
     token_select_functionality();
-
-
-    
-
-
-
-
+    start_game_functionality();
+    reset_functionality();
 });
 
 
@@ -37,5 +30,37 @@ const token_select_functionality = function(){
             event.target.classList.toggle("chosen");
         })
     });
+}
 
+
+const start_game_functionality = function(){
+    const startGame = document.getElementById("start");
+    startGame.addEventListener("click", () => {
+        if(localStorage.getItem("MINIMAX-SOLVER")){
+            document.getElementById("canvas").classList.toggle("in-session");
+            return;
+        }
+        window.alert("Choose a token first duh");
+        return;
+    });
+}
+
+const reset_functionality = function(){
+    const reset = document.getElementById('reset');
+    reset.addEventListener("click", () => {
+        if(localStorage.getItem("MINIMAX-SOLVER")){
+            const previous = document.getElementById(String(JSON.parse(localStorage.getItem("MINIMAX-SOLVER"))["token"]));
+            previous.classList.toggle("chosen");
+            localStorage.removeItem("MINIMAX-SOLVER");
+            document.getElementById("canvas").classList.toggle("in-session");
+            if(!localStorage.getItem("MINIMAX-SOLVER")){
+                window.alert("successfully reset storage");
+                return;
+            }
+            window.alert("error: could not remove storage !");
+            return;
+        }
+        window.alert("nothing to reset");
+        return;
+    });
 }
