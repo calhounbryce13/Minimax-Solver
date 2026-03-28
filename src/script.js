4916b86733576bac1847e8861a4e808b5222cb90
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2026-03-27 23:29:43 -0700

    cell click functionality, starting the game

--- src/script.js
+++ src/script.js
@@ -3,10 +3,39 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    localStorage.removeItem("MINIMAX-SOLVER--TURN");
+
     check_local_storage();
     token_select_functionality();
     start_game_functionality();
     reset_functionality();
+
+
+
+
+
+    const cells = Array.from(document.getElementsByClassName("cell"));
+    cells.forEach((cell) => {
+        cell.addEventListener("click", (event) => {
+            if(localStorage.getItem("MINIMAX-SOLVER--TURN")){
+                if(Number(JSON.parse(localStorage.getItem("MINIMAX-SOLVER--TURN"))) == 1){
+                    const element = event.target;
+                    if(!(element.classList.contains("x-tile-icon")) && !(element.classList.contains("o-tile-icon"))){
+                        const token = String(JSON.parse(localStorage.getItem("MINIMAX-SOLVER"))["token"]);
+                        const tokenClass = (token == "x") ? "x-tile-icon" : "o-tile-icon";
+                        element.classList.toggle(tokenClass);
+                        return;
+                    }
+                    window.alert("invalid tile, chose an empty square");
+                    return;
+                }
+                window.alert("not your turn");
+                return;
+            }
+            window.alert("start the game to play a tile");
+            return;
+        });
+    });
 });
 
 
@@ -36,8 +65,11 @@ const token_select_functionality = function(){
 const start_game_functionality = function(){
     const startGame = document.getElementById("start");
     startGame.addEventListener("click", () => {
-        if(localStorage.getItem("MINIMAX-SOLVER")){
+        const tokenObj = localStorage.getItem("MINIMAX-SOLVER");
+        if(tokenObj){
             document.getElementById("canvas").classList.toggle("in-session");
+            const whosturn = (String(JSON.parse(tokenObj)["token"]) == "o") ? 0 : 1;
+            localStorage.setItem("MINIMAX-SOLVER--TURN", JSON.stringify(whosturn))
             return;
         }
         window.alert("Choose a token first duh");
@@ -52,7 +84,10 @@ const reset_functionality = function(){
             const previous = document.getElementById(String(JSON.parse(localStorage.getItem("MINIMAX-SOLVER"))["token"]));
             previous.classList.toggle("chosen");
             localStorage.removeItem("MINIMAX-SOLVER");
-            document.getElementById("canvas").classList.toggle("in-session");
+            localStorage.removeItem("MINIMAX-SOLVER--TURN");
+            if(document.getElementById("canvas").classList.contains("in-session")){
+                document.getElementById("canvas").classList.remove("in-session");
+            }
             if(!localStorage.getItem("MINIMAX-SOLVER")){
                 window.alert("successfully reset storage");
                 return;
